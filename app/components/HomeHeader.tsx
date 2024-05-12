fb90d74c2c45bf5b53d6054ca38611f48df1c96c
abdulrahmanmahmood <abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2024-05-12 03:29:47 +0300

    edit the secound scroll bar

---
+++ app/components/HomeHeader.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const HomeHeader = () => {
+  const names = [
+    {
+      name: "All",
+    },
+    {
+      name: "The hole of Quran",
+    },
+    {
+      name: "All",
+    },
+    {
+      name: "Amazing Nasheed Compilation",
+    },
+    {
+      name: "Beautiful Recitation of Surah Al-Fatiha",
+    },
+    {
+      name: "Quranic Stories: Prophet Yusuf (AS)",
+    },
+    {
+      name: "Tafsir of Surah Al-Baqarah",
+    },
+    {
+      name: "Islamic History: The Life of Prophet Muhammad (PBUH)",
+    },
+    {
+      name: "Islamic Lectures: Understanding the Five Pillars of Islam",
+    },
+    {
+      name: "Islamic Reminders: Importance of Prayer",
+    },
+    {
+      name: "Islamic Stories: The Story of Maryam (AS)",
+    },
+    {
+      name: "Islamic Documentaries: The Kaaba",
+    },
+    {
+      name: "Islamic Events: Hajj Journey 2023",
+    },
+    {
+      name: "Islamic Talks: The Importance of Sadaqah",
+    },
+    {
+      name: "Islamic Debates: Is Music Permissible in Islam?",
+    },
+    {
+      name: "Islamic Education: Learning Arabic for Quranic Understanding",
+    },
+    {
+      name: "Islamic Poetry: Reflections on Jannah",
+    },
+    {
+      name: "Islamic Scholar Interviews: Interview with Mufti Menk",
+    },
+    {
+      name: "Islamic Q&A: Fatwas on Contemporary Issues",
+    },
+  ];
+
+  // Function to trim titles to maximum of three words
+
+  return (
+    <div className="flex flex-row overflow-x-scroll w-[80&] mx-auto scroll scrollbar-none">
+      {names.map((video, index) => (
+        <div
+          key={index}
+          className="px-3 py-1 text-nowrap bg-[#FF0000] mx-2 rounded-2xl"
+        >
+          {video.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HomeHeader;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import HomeHeader from "./components/HomeHeader";
 
 export default function Home() {
   return (
     <div className="bg-[#11112E] w-[100%] min-h-[100vh]  ">
       <div className="flex flex-col items-center justify-center">
         <Header />
+
         <div className="flex flex-row w-full">
           <Sidebar />
-          <div className="h-full"></div>
+          <div className="h-full overflow-hidden w-[80%]">
+            <HomeHeader/>
+          </div>
         </div>
       </div>
     </div>
